03e0468205712579691c6d26ddfae37628387749
Nate080 <nate080@github.com>
2025-04-05 21:25:36 -0400

    Clean up workspace and fix CORS issues with mock data

--- run_bbb.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "C:\Users\natha\New folder"
-powercfg /change standby-timeout-ac 0
-powercfg /change hibernate-timeout-ac 0
-"C:\Program Files\nodejs\node.exe" --unhandled-rejections=strict bbb.js
-pause 
